ba03365e001a27306e4bf54714a318ac249ca3d9
Govardhan8 <govardhanmurthy007@gmail.com>
2023-05-02 21:24:37 +0530

    Adding divider

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Info from "./info";
 import mypic from "../../assets/mypic.jpg";
+import Divider from "../utils/divider";
 
 const index = () => {
   return (
-    <section className="flex flex-col md:flex-row md:flex-row-reverse">
-      <img
-        src={mypic}
-        alt="display pic"
-        className="w-[320px] md:w-4/12 aspect-square object-contain md:rounded-3xl"
-      />
-      <Info />
+    <section>
+      <article className="flex flex-col md:flex-row md:flex-row-reverse">
+        <img
+          src={mypic}
+          alt="display pic"
+          className="w-[320px] md:w-4/12 aspect-square object-contain md:rounded-3xl"
+        />
+        <Info />
+      </article>
+      <Divider />
     </section>
   );
 };
---
+++ src/components/utils/divider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const divider = () => {
+  return <div className="my-3 h-px w-full bg-gray-100" />;
+};
+
+export default divider;
